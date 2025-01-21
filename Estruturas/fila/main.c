3415eba67fc79ae27747ea7639bb9503c2cd39bf
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-21 20:26:22 -0300

    adicionando estruturas básicas para a fila

--- Estruturas/fila/main.c
+++ Estruturas/fila/main.c
@@ -3,7 +3,27 @@
 
 // Estrutura de dado: Fila
 
+/*
+A estrutura de dados "fila" também chamado de "queue", segue
+o princípio da fifo (first in first out) onde o primeiro a
+chegar também é o primeiro a sair, semelhante a uma fila mesmo.
+*/
+
+struct fifo
+{
+    int num;
+    int array[10];
+}queue;
+
+void addItem(){
+
+}
+
+void removeItem(){
+
+}
+
 int main(int argc, char *argv[]){
-    printf("Hello World!");
+    
     return 0;
 }
