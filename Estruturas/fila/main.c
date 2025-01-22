8d110f2b88973b5738eeeede165d90b8dc3fa0ec
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-22 12:59:28 -0300

    adicionando função de addItem, mas precisa de melhorias.

--- Estruturas/fila/main.c
+++ Estruturas/fila/main.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+#define SIZE 5
 
 // Estrutura de dado: Fila
 
@@ -10,20 +11,49 @@ chegar também é o primeiro a sair, semelhante a uma fila mesmo.
 */
 
 struct fifo
-{
+{   
     int num;
-    int array[10];
+    int front;
+    int last;
+    int array[SIZE];
 }queue;
 
-void addItem(){
-
+// iniciar o array com -1 pra sinalizar que não possui nada
+void initQueue(){
+    queue.front = -1;
+    queue.last = -1;
 }
 
-void removeItem(){
+void addItem(int val){
+    // Caso a vetor estiver cheio
+    if(queue.last != -1){
+        printf("Array is full");
+    }
+    
+    // Caso a vetor estiver vazia;
+    if(queue.front == -1){
+        queue.front = 0;
+    }
+    // Adicionando valor ao vetor
+    queue.array[queue.front] = val;
+    printf("Adding value %d in array",val);
+}
+
+void removeItem(int val){
 
 }
 
 int main(int argc, char *argv[]){
-    
+    // inicio do código
+
+    printf("Input numbers:\n");
+    for(int i = 0;i < SIZE; i++){
+        scanf("%d",queue.num);
+        addItem(queue.num);    
+    }
+
+    for(int i = 0;i < SIZE;i++){
+        printf("values: %d\n",queue.array[i]);
+    }
     return 0;
 }
