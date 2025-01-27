7a6bc3a847c06c541ef3061b6804435dbab4753e
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-27 15:08:56 -0300

    Adicionado lógica da criação da pilha, da adição de elemento e da remoção.

--- Estruturas/pilha/pilha.c
+++ Estruturas/pilha/pilha.c
@@ -5,7 +5,11 @@
 
 // Criando a pilha.
 Stack* createStack(int capacity){
-
+    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    stack->capacity = capacity;
+    stack->array = (int*)malloc(sizeof(capacity*sizeof(int)));
+    stack->top = -1;
+    return stack;
 }
 
 // verificar se está vazia
@@ -25,12 +29,20 @@ void freeStack(Stack* stack){
 
 // Adiciona o elemento na pilha
 void push(Stack* stack, int element){
-
+    // Verificando se a pilha está cheia
+    if(isFull(stack)){
+        return printf("Stack is full, try remove at least one element!");
+    }
+    stack->array[stack->top] = element;
 }
 
 // Remove o elemento do topo da pilha
 int pop(Stack* stack){
-
+    // Verificando se a pilha está vazia.
+    if(isEmpty(stack)){
+        return printf("Stack it's empty!");
+    }
+    return stack->array[stack->top] -= stack->top;
 }
 
 // Mostra o elemento no topo da pilha
