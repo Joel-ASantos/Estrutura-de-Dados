cbc4b5b5a30c8bd36d357022b56be101f911ecf6
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-27 12:30:43 -0300

    Adicionando as funçõe para a pilha

--- Estruturas/pilha/pilha.c
+++ Estruturas/pilha/pilha.c
@@ -2,3 +2,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "pilha.h"
+
+// Criando a pilha.
+Stack* createStack(int capacity){
+
+}
+
+// verificar se está vazia
+bool isEmpty(Stack* stack){
+
+}
+
+// verificar se está cheia
+bool isFull(Stack* stack){
+    
+}
+
+// libera a memória da pilha
+void freeStack(Stack* stack){
+    
+}
+
+// Adiciona o elemento na pilha
+void push(Stack* stack, int element){
+
+}
+
+// Remove o elemento do topo da pilha
+int pop(Stack* stack){
+
+}
+
+// Mostra o elemento no topo da pilha
+int peek(Stack* stack){
+
+}
+
+// tamanho da pilha
+int size(Stack* stack){
+
+}
