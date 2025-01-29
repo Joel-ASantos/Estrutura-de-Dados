01b57da23b538b3e39ceb5e7ce05b7ce5af679ef
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-29 12:16:48 -0300

    adicionando funções e corrigindo nome do removeElement

--- Estruturas/hash/hash.c
+++ Estruturas/hash/hash.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "hash.h"
+
+// Funções para o Hash Map
+
+int insertElement(Hash_Table* table, int key, const char* object){
+
+}
+
+char* searchElement(Hash_Table* table,int key){
+
+}
+
+void removeElement(Hash_Table* table, int key){
+
+}
+
+void freeMemory(Hash_Table* table){
+
+}
--- Estruturas/hash/hash.h
+++ Estruturas/hash/hash.h
@@ -18,7 +18,7 @@ typedef struct{
 
 int insertElement(Hash_Table* table, int key, const char* object);
 char* searchElement(Hash_Table* table, int key);
-void removeElement7(Hash_Table* table, int key);
+void removeElement(Hash_Table* table, int key);
 void freeMemory(Hash_Table* table);
 
 #endif
