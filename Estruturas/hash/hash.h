856914eea65efcb6aac271d472e062a7fb135485
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-28 21:07:29 -0300

    adicionando inteface para construção do hash map.

---
+++ Estruturas/hash/hash.h
@@ -0,0 +1,24 @@
+// Interface para a tabela HASH
+
+#ifndef HASH_MAP_H
+#define HASH_MAP_H
+
+#define HASH_SIZE 16
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct Hash{
+    int key;
+    char* object;
+}Hash;
+
+typedef struct{
+    Hash* bucket[HASH_SIZE];
+}Hash_Table;
+
+int insertElement(Hash_Table* table, int key, const char* object);
+char* searchElement(Hash_Table* table, int key);
+void removeElement7(Hash_Table* table, int key);
+void freeMemory(Hash_Table* table);
+
+#endif
---
+++ Estruturas/hash/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "hash.h"
+
+// Estrutura de Dados: Hash Map
+
+// A estrutura de dados hash map, ou também conhecida
+// como tabela hash, é conhecida pelo capacidade de armazenar
+// pares de chave-valor. É uma forma bastante eficiente de
+// acessar os valores associados às chaves.
+
+int main(int* argc, char argv[]){
+    
+    return 0;
+}
