7d62534f2b97c89ef3bd0c56fb4b4294c47dda19
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-30 15:00:05 -0300

    adicionando as funções da árvore.

--- Estruturas/arvores/busca/tree.c
+++ Estruturas/arvores/busca/tree.c
@@ -3,3 +3,28 @@
 #include "tree.h"
 
 // Funções para árvore binária de busca
+
+// Criação dos nós da árvore
+Node* createNodes(int value){
+
+}
+
+// Função para encontrar um valor especifico na árvore
+Node* find(Node* root, int value){
+
+}
+
+// Função para inserção de valores para à árvore
+void insert(Node* root, int value){
+
+}
+
+// Função para deletar um elemento na árvore
+void delete(Node* root, int value){
+
+}
+
+// Função para imprimir à árvore
+void printfTree(Node* root){
+
+}
