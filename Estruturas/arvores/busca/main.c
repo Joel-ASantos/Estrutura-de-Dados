429ec6484a7815037bb89f1867e3c573da789d34
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-30 13:03:18 -0300

    adicionado a interface no tree.h para a BST, e adicionando os demais arquivos.

---
+++ Estruturas/arvores/busca/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+// Estrutura de Dados: Árvore Binária de Busca
+
+int main(int* argc, char argv[]){
+    
+    return 0;
+}
---
+++ Estruturas/arvores/busca/tree.h
@@ -0,0 +1,6 @@
+// interface para à arvore
+#ifdef BINARY_SEARCH_TREE
+
+typedef st
+
+#endif
