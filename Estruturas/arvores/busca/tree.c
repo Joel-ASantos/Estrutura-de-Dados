5af4b711509e8adf732f0ed3f000510c6006b165
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-30 20:21:47 -0300

    lógica do insert implementada

--- Estruturas/arvores/busca/tree.c
+++ Estruturas/arvores/busca/tree.c
@@ -6,7 +6,11 @@
 
 // Criação dos nós da árvore
 Node* createNodes(int value){
-
+    Node* no = (Node*)malloc(sizeof(Node));
+    no->head = NULL;
+    no->right = NULL;
+    no->left = NULL;
+    no->value = value;
 }
 
 // Função para encontrar um valor especifico na árvore
@@ -16,7 +20,24 @@ Node* find(Node* root, int value){
 
 // Função para inserção de valores para à árvore
 void insert(Node* root, int value){
+    // Inserindo valores
+    // No entanto, preciso verificar se existe uma raíz para iniciar o processo
+    // Caso não exista a cabeça ou "raíz" vai ser receber um valo.
+    if(root->head == NULL) root->head = value;
 
+    // Começar o processo de alocar o valor com no seu devido lugar
+    /*
+        if(root->left == NULL && value < root->head){
+            root->left = value;
+        }else if(root->right == NULL && value > root->head){
+            root->right = value;
+        }
+    */
+    if(root->left == NULL && value < root->head){
+        insert(&(root->left),value);
+    }else if(root->right == NULL && value > root->head){
+        insert(&(root->right),value);
+    }
 }
 
 // Função para deletar um elemento na árvore
