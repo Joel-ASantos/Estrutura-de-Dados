8914dc180d94f09d90a8039f0fd20300d5969595
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-02-01 13:37:47 -0300

    adicionado lógica da busca

--- Estruturas/arvores/busca/tree.c
+++ Estruturas/arvores/busca/tree.c
@@ -15,12 +15,19 @@ Node* createNodes(int value){
 
 // Função para encontrar um valor especifico na árvore
 Node* find(Node* root, int value){
-
+    if(root == NULL || root->value == value) return root;
+    
+    if(value < root->value){
+        return find(root->left,value);
+    }else if(value > root->value){
+        return find(root->right,value);
+    }
 }
 
 // Função para inserção de valores para à árvore
 void insert(Node* root, int value){
     // Inserindo valores
+
     // No entanto, preciso verificar se existe uma raíz para iniciar o processo
     // Caso não exista a cabeça ou "raíz" vai ser receber um valo.
     if(root->head == NULL) root->head = value;
@@ -42,7 +49,24 @@ void insert(Node* root, int value){
 
 // Função para deletar um elemento na árvore
 void delete(Node* root, int value){
+    // Deletando valores
 
+    // Alguns casos a se considerar, caso o eu remover um elemento
+    // que é pai de um "filho", a estrutura da árvore muda.
+    // Se for somente os filhos a serem removidos, a estrutura da árvore permanece.
+    
+    // Verifica se a árvore já está vazia
+    if(root == NULL) printf("\nRoot is empty");
+
+    // Processo de remoção dos filhos
+    if(root->head != NULL && root->head == value){ // Encontrar o menor nó dentro do lado direito para substituir o head
+
+    }
+    else if(root->left != NULL && root->left == value){
+
+    }else if(root->right != NULL && root->right == value){
+
+    }
 }
 
 // Função para imprimir à árvore
