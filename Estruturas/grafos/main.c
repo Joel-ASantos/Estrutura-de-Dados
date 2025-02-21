295a981abb1feb9a56ce419b04979c87e85a4aa7
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-02-21 14:21:42 -0300

    adicionando função do addedges

---
+++ Estruturas/grafos/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Estrutura de Dados: Grafo
+
+// À estrutura de dados "grafo", é uma estrutura de dados
+// não linear, que consiste de vertices (nós) e bordas (edges)
+// que conectam pares de nós.
+
+void Addedges(){
+
+}
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
